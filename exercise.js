89cb81054197b579bb4e55a90d6cd4423a587510
GabrielFurtado <gabriel.f.salarini@gmail.com>
2018-07-09 20:44:04 -0300

    first function

---
+++ exercise.js
@@ -0,0 +1,10 @@
+console.log ("CONNECTED");
+
+function printReverse(arr) {
+    console.log ("PRINT REVERSE FUNCTION:");
+    for(var i = arr.length -1; i>=0; i--) {
+        console.log (arr[i]);
+    }
+}
+
+
